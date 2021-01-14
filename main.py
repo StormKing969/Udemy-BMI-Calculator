d26ac5c71f8d0bb421775a06a9395a3cc56660f8
StormKing969 <wijesinghesaj@gmail.com>
2021-01-13 22:46:17 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -5,9 +5,32 @@ weight = int(input("Enter your weight: "))
 height = int(input("Enter your height: "))
 
 en_bmi = weight / (height ** 2)
-us_bmi = (weight * 0.45359237) / ((height * 3.2808) ** 2)
+us_bmi = int((weight * 0.45359237) / ((height / 3.2808) ** 2))
 
 if unit == "yes":
-    print("Your BMI is : " + str(int(us_bmi)))
+    if us_bmi < 18.5:
+        print(f"Your BMI is {us_bmi} and you are underweight")
+    elif 25 > us_bmi >= 18.5:
+        print(f"Your BMI is {us_bmi} and you are normal weight")
+    elif 25 <= us_bmi < 30:
+        print(f"Your BMI is {us_bmi} and you are overweight")
+    elif 30 <= us_bmi < 35:
+        print(f"Your BMI is {us_bmi} and you are obese")
+    else:
+        print(f"Your BMI is {us_bmi} and you are clinically obese")
 else:
-    print("Your BMI is : " + str(int(en_bmi)))
+    if en_bmi < 18.5:
+        print("Your BMI is : " + str(int(en_bmi)))
+        print("You are underweight")
+    elif 25 > en_bmi >= 18.5:
+        print("Your BMI is : " + str(int(en_bmi)))
+        print("You are normal weight")
+    elif 25 <= en_bmi < 30:
+        print("Your BMI is : " + str(int(en_bmi)))
+        print("You are overweight")
+    elif 30 <= en_bmi < 35:
+        print("Your BMI is : " + str(int(en_bmi)))
+        print("You are obese")
+    else:
+        print("Your BMI is : " + str(int(en_bmi)))
+        print("You are clinically obese")
